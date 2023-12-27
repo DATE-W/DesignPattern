687db3c6ada6539a1ef5ddfaf25fd95ebc2e9ca8
LevisT <1329361431@qq.com>
2023-12-28 02:38:06 +0800

    添加生成原料示例

--- code/framework/food/TacoFactory.java
+++ code/framework/food/TacoFactory.java
@@ -3,13 +3,23 @@ package framework.food;
 import framework.food.item.Item;
 import framework.food.item.taco.*;
 import framework.food.setMeal.SetMeal;
+import framework.ingredient.IngredientFactory;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 
 class TacoFactory extends FoodFactory{
+    /**
+     * 获取单例对象
+     */
+    IngredientFactory ingredientFactory=IngredientFactory.getInstance();
     @Override
     public Item createItem(FoodName foodName) {
+        /**
+         * 产生原料
+         * 以芝士为例
+         */
+        ingredientFactory.createIngredient(IngredientFactory.IngredientType.芝士);
         switch (foodName) {
             case CheeseFiredChickenTaco:
                 return new CheeseFiredChickenTaco(
