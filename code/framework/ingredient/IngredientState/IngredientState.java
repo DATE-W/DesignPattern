3173066a91fa39bfeeab54b7e7ab3992d36afe2f
LevisT <1329361431@qq.com>
2024-01-02 01:04:17 +0800

    genshin

--- code/framework/ingredient/IngredientState/IngredientState.java
+++ code/framework/ingredient/IngredientState/IngredientState.java
@@ -9,11 +9,6 @@ import framework.ingredient.Ingredient;
  * 声明不同状态食材的方法
  */
 public interface IngredientState {
-//    protected Ingredient ingredient;
-//    public void  setIngredientState(Ingredient ingredient){
-//        this.ingredient=ingredient;
-//    }
-
     public abstract void showState();
 
     public abstract String getState();
